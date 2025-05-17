f78c24d57d53bb38ae409bee751ae2bff4f49770
daniel <dannysakyi@gmail.com>
2025-05-16 20:44:45 -0400

    type command

--- app/main.js
+++ app/main.js
@@ -22,6 +22,16 @@ let prompt = () => {
     } else if(answer.includes("echo")){
       let ans = answer.replace("echo ", "");
       console.log(ans)
+    }else if(answer.includes("type")){
+      if(answer.includes("echo")){
+        console.log(`echo is a shell builtin`)
+      } else if(answer.includes("exit")){
+        console.log(`exit is a shell builtin`)
+      } else if(answer.includes("invalid_command")){
+        console.log(`invalid_command: not found`)
+
+      }
+      
     }else{
       console.log(`${answer}: command not found`)
     }
